d86ed13785ae1888f663aac04f84a359e2098dc5
daviderino <david.baptista00@hotmail.com>
2020-12-03 10:32:22 +0000

    final v2

--- olap.sql
+++ olap.sql
@@ -9,6 +9,8 @@ GROUPING SETS((a.especialidade, ano, mes), (a.especialidade, ano))
 ORDER BY ano, mes;
 
 
+
+
 CREATE VIEW new_view AS
 (SELECT v.substancia, c.nome, dt.mes, dt.semana, dt.dia_da_semana, sum(v.quant) total, count(1) numero_de_vendas
 FROM f_presc_venda fp
@@ -20,9 +22,11 @@ JOIN d_tempo dt ON dt.id_tempo = fp.id_data_registo
 WHERE dt.ano = 2020 AND dt.trimestre = 1 AND r.nome = 'Lisboa'
 GROUP BY v.substancia, c.nome, dt.mes, dt.semana, dt.dia_da_semana);
 
-SELECT substancia, nome concelho, mes, semana, sum(total) AS total, avg(numero_de_vendas) AS media
+/* Como não temos registos para todos os dias da semana e todos os meses, a função avg() não irá devolver a média diária exata */
+
+SELECT substancia, nome concelho, mes, dia_da_semana, sum(total) AS total, avg(numero_de_vendas) AS media
 FROM new_view 
 GROUP BY
 substancia,
-ROLLUP(mes, semana),
+ROLLUP(mes, dia_da_semana),
 ROLLUP(nome);
